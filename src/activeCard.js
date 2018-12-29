ede082eefd486b7fe902e8a08746e7df12063671
Dovydas Venckus <dovydas.venckus@live.com>
2018-12-29 19:04:45 +0200

    Change style to use dark background and green cards with white text

--- src/activeCard.js
+++ src/activeCard.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Link } from "react-router-dom";
 
 const ActiveCard = props => {
   return (
-    <div className="activeCard">
+    <div className="card activeCard">
       <p>{props.match.params.id}</p>
     </div>
   );
--- src/scrumPoker.js
+++ src/scrumPoker.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Card from "./card";
 
 class ScrumPoker extends React.Component {
